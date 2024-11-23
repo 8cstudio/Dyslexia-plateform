ac36b9f9522077783e1e5bc3bcb0313cbba33a09
zarshab <zarshabrafiq1092@gmail.com>
2024-11-23 20:54:33 +0500

    updated ui

--- config/database.js
+++ config/database.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
-const MONGO_URI = process.env.MONGO_URI;
 
 // Function to Connect to MongoDB
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected successfully.");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
